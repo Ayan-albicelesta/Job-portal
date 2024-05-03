db38cfe0a8adb8877c22664718aebdabb3d1fdbb
Ayan khanra <ayan.khanracoder2001@gmail.com>
2024-05-03 12:17:49 +0530

    changes made

---
+++ config/dbConnect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+async function connectDB(){
+     try {
+         const connection= await mongoose.connect(process.env.DB_URL);
+         console.log(`DB connected ${mongoose.connection.host}`);
+     } catch (error) {
+        console.log("DB connection error ",error);
+     }
+}
+ 
+export default connectDB;
+
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import dotenv from 'dotenv';
+import connectDB from './config/dbConnect.js';
+dotenv.config() //dotenv.config({path:''}) --if .env file was created inside anither folder instead root folder we had to invoke path like here
+
+const PORT= process.env.PORT || 8080;
+
+//database connection
+connectDB();
+ 
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome to My job portal</h1>');
+})
+
+app.listen(8080,()=>{
+    console.log(`Node server is running in ${process.env.DEV_MODE} mode on port ${process.env.PORT}` );
+})
