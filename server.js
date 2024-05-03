393428404bbf69b02dd2305f00f0674ea083b922
Ayan khanra <ayan.khanracoder2001@gmail.com>
2024-05-03 18:25:43 +0530

    check

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.get('/',(req,res)=>{
     res.send('<h1>Welcome to My job portal</h1>');
 })
 
-app.listen(8080,()=>{
+app.listen(808,()=>{
     console.log(`Node server is running in ${process.env.DEV_MODE} mode on port ${process.env.PORT}` );
 })
